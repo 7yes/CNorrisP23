c8ece67787795f0410afe3055a0fc162dde005ea
7yes <100316346+7yes@users.noreply.github.com>
2023-08-21 10:45:57 -0500

    nav_graph, nav_bottom

--- app/src/main/java/com/example/cnorrisp/ui/activies/MainActivity.kt
+++ app/src/main/java/com/example/cnorrisp/ui/activies/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.cnorrisp.ui.activies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.cnorrisp.R
+import com.example.cnorrisp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.frag_container) as NavHostFragment? ?: return
+
+         // Set up Action Bar
+        val navController = host.navController
+        //setupBottomNavMenu(navController)
+        binding.navBotMenu.setupWithNavController(navController)
     }
 }
